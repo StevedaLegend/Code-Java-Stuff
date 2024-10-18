7cab847f5b0e5599d4b9611c824ee3443ecd6c92
C00275756 Steve Fasoranti <113533879+StevedaLegend@users.noreply.github.com>
2024-10-18 21:23:37 +0100

    First attempt at checking for a number in an array
    Its my first attempt at a java code I did time ago and decided to put it into visual studio and it was all messy (lol) I will now attempt at fixing it!

---
+++ Small Code Excersises/Tutorial Sheet #1/CheckArray.java
@@ -0,0 +1,21 @@
+public class CheckArray {
+
+    Public
+    static void main(args[]string){
+
+
+
+        string = [8];
+        array = string;
+        int = 7;
+
+
+        for (i => 0; array.length i++){
+
+            if array = 7;
+            return 7;
+            else
+            return null
+        }
+    }
+}
